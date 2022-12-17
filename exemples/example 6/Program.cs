c38a8c983dd251c357d63190b901a5c9ed7735e8
Alexander <student20042004@inbox.ru>
2022-12-17 13:02:30 +1000

    задача с гирями

---
+++ exemples/example 6/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 8;
+int c = 2;
+int d = 5;
+int e = 7;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
+ 
