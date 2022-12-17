42850878d6de17c540fb7ed905ae7f44c32f3605
Alexander <student20042004@inbox.ru>
2022-12-17 11:00:13 +1000

    add git ignore

---
+++ exemples/exemple 2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Hello,");
+Console.Write(username);
