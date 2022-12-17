f4e656eee9010bb599ba93cd63e2a34bab084093
Alexander <student20042004@inbox.ru>
2022-12-17 10:18:03 +1000

    hello world

--- exemples/exemple 1/obj/Debug/net6.0/exemple 1.AssemblyInfo.cs
+++ exemples/exemple 1/obj/Debug/net6.0/exemple 1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("test progr")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("exemple 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("test progr")]
-[assembly: System.Reflection.AssemblyTitleAttribute("test progr")]
+[assembly: System.Reflection.AssemblyProductAttribute("exemple 1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("exemple 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
